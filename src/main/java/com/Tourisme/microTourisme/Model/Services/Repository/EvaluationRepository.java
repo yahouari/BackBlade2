25be39ff315f9e2bb1b09a549ce923fe9e807acf
YoussefChana <59029575+YoussefChana@users.noreply.github.com>
2020-06-05 15:08:51 +0100

    Update EvaluationRepository.java

--- src/main/java/com/Tourisme/microTourisme/Model/Services/Repository/EvaluationRepository.java
+++ src/main/java/com/Tourisme/microTourisme/Model/Services/Repository/EvaluationRepository.java
@@ -1,12 +1,17 @@
 package com.Tourisme.microTourisme.Model.Services.Repository;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import com.Tourisme.microTourisme.Model.Services.Voyage;
+
+import com.Tourisme.microTourisme.Model.Services.Evaluation;
+
 
 @CrossOrigin("*")
-public interface EvaluationRepository extends JpaRepository<Voyage,Integer>  {
-
+public interface EvaluationRepository extends JpaRepository<Evaluation,Integer>  {
+	
 }
+
